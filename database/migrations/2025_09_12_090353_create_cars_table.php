7ccf945a212fdefeced6a5be9bbfd3e50333f069
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-09-12 11:21:13 +0200

    ajout colonnes cars

--- database/migrations/2025_09_12_090353_create_cars_table.php
+++ database/migrations/2025_09_12_090353_create_cars_table.php
@@ -13,6 +13,26 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('brand_id')->constrained('brands');
+            $table->foreignId('fuel_id')->constrained('fuels');
+            $table->string('model');
+            $table->enum('etat', ['neuf','occasion']);
+            $table->year('annee');
+            $table->unsignedBigInteger('kilometrage');
+            $table->boolean('abs')->default(false);
+            $table->string('image1_path'); 
+            $table->string('image2_path')->nullable();
+            $table->string('image3_path')->nullable();
+            $table->string('image4_path')->nullable();
+            $table->enum('jantes', ['16','17','18','19','NONE']);
+            $table->enum('sellerie', ['Cuir','Tissus']);
+            $table->string('couleur', 7);
+            $table->enum('type', ['4X4','SUV','BREAK','LUDOSPACE','VAN','BERLINE']);
+            //  SI VOITURE ELEC CYLINDREE = NONE, OUBLIE PAS 
+            $table->enum('cylindree', ['1l','1.2l','1.5l','1.8l','2l','3l','NONE']);
+            $table->decimal('prix', 12, 2);
+            $table->text('description');
             $table->timestamps();
         });
     }
