125065123f4fd616438a054eb675bd878fd69998
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-09-12 11:11:05 +0200

    ajout colonnes brands

--- database/migrations/2025_09_12_090259_create_avatars_table.php
+++ database/migrations/2025_09_12_090259_create_avatars_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('avatars', function (Blueprint $table) {
             $table->id();
-            $table->string('path');
+            $table->string('avatar_path')->nullable();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
