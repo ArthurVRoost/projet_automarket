2c301a7665a1ad80dc33f521516f748ad23320fd
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-09-12 11:58:55 +0200

    page home div search

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -7,7 +7,9 @@ export default function Home({auth, user}) {
     <div>
       <Nav auth={auth} user={user}/>
       <div className='divHome'>
-        <h1>page home</h1>
+        <h1 style={{marginTop:'80px'}}>Trouvez votre véhicule idéal</h1>
+        <p>Des millieurs d'annonces vérifiées à votre disposition</p>
+        <input type="text" placeholder='Rechercher par marque, modèle...' className='inputHome' name="" id="" />
       </div>
       
       <Footer/>
