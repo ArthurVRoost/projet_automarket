15c9434e1904cda474ac2e2255ffea565c092cff
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-09-15 11:52:10 +0200

    footer fini

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -1,97 +1,21 @@
+import { Link } from '@inertiajs/react';
 import React from 'react'
-
+import { LuCar } from "react-icons/lu";
+import '../../css/app.css'
 export default function Footer () {
   return (
-    <section className="">
-  {/* Footer */}
-  <footer className="bg-body-tertiary text-center">
-    {/* Grid container */}
-    <div className="container p-4">
-      {/*Grid row*/}
-      <div className="row">
-        {/*Grid column*/}
-        <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-          <h5 className="text-uppercase">Footer Content</h5>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque
-            ea quis molestias. Fugiat pariatur maxime quis culpa corporis vitae
-            repudiandae aliquam voluptatem veniam, est atque cumque eum delectus
-            sint!
-          </p>
+    <>
+      <div className='divFooter'>
+        <div className='div1Footer'>
+          <Link style={{textDecoration:"none", color:'white'}} className='div1FooterP'><LuCar/> Automarket</Link>
+          <Link href={'/'} style={{textDecoration:"none", color:'white'}} className='div1FooterLink'>Catalogue</Link>
+          <Link href={'/'}style={{textDecoration:"none", color:'white'}} className='div1FooterLink'>Vendez votre voiture</Link>
         </div>
-        {/*Grid column*/}
-        {/*Grid column*/}
-        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase">Links</h5>
-          <ul className="list-unstyled mb-0">
-            <li>
-              <a href="#!" className="text-body">
-                Link 1
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="text-body">
-                Link 2
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="text-body">
-                Link 3
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="text-body">
-                Link 4
-              </a>
-            </li>
-          </ul>
-        </div>
-        {/*Grid column*/}
-        {/*Grid column*/}
-        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase mb-0">Links</h5>
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!" className="text-body">
-                Link 1
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="text-body">
-                Link 2
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="text-body">
-                Link 3
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="text-body">
-                Link 4
-              </a>
-            </li>
-          </ul>
-        </div>
-        {/*Grid column*/}
       </div>
-      {/*Grid row*/}
-    </div>
-    {/* Grid container */}
-    {/* Copyright */}
-    <div
-      className="text-center p-3"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-    >
-      © 2020 Copyright:
-      <a className="text-body" href="https://mdbootstrap.com/">
-        MDBootstrap.com
-      </a>
-    </div>
-    {/* Copyright */}
-  </footer>
-  {/* Footer */}
-</section>
+      <div className='div2Footer'>
+            <p className='copyright'> Copyright &copy; - Arthur Van Roost 2025 </p>
+        </div>
+    </>
 
   )
 }
