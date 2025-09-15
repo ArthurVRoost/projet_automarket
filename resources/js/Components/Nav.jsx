256e90783e76d62ae5963604607737a5958770ca
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-09-15 11:35:47 +0200

    changement nav

--- resources/js/Components/Nav.jsx
+++ resources/js/Components/Nav.jsx
@@ -2,32 +2,31 @@ import { Link, router } from '@inertiajs/react'
 import React from 'react'
 import { LuCar } from "react-icons/lu";
 import { GoGear, GoPlus } from "react-icons/go";
-
+import '../../css/app.css'
 export default function Nav ({auth, user}) {
   return (
     <div style={{display: 'flex', alignItems: 'center', gap: '10px'}}>
       {auth.user ? (
-        <div style={{padding: '10px', display: 'flex', alignItems: 'center', gap: '20px'}}>
+        <div className='navDiv' >
           <h3 style={{color: '#3468df', display: 'flex', alignItems: 'center', gap: '5px'}}>
             <LuCar /> AutoMarket
           </h3>
-          <p>Catalogue</p>
-          <p style={{display: 'flex', alignItems: 'center', gap: '5px'}}>
-            <GoPlus /> Vendez votre voiture
-          </p>
-          <p style={{display: 'flex', alignItems: 'center', gap: '5px'}}>
-            <GoGear /> Administration
-          </p>
-          <button onClick={() => router.post(route('logout'))}>Déco</button>
+          <Link className='LinkNav' href={'/'} >Catalogue</Link>
+          <Link className='LinkNav' href={'/'}><GoPlus /> Vendez votre voiture</Link>
+          <Link className='LinkNav' href={'/admin'} ><GoGear/> Administration</Link>
+          <button className='btnRegister' onClick={() => router.post(route('logout'))}>Déconnexion</button>
         </div>
       ) : (
-        <div style={{padding: '10px', display: 'flex', alignItems: 'center', gap: '40px'}}>
+        <div className='navDiv'>
           <h3 style={{color: '#3468df'}}><LuCar /> AutoMarket</h3>
-          <p>Catalogue</p>
-          <p><GoPlus /> Vendez votre voiture</p>
-          <p><GoGear /> Administration</p>
-          <Link href={route('login')}>Login</Link>
-          <Link href={route('register')}>Register</Link>
+          <Link className='LinkNav' href={'/'} >Catalogue</Link>
+          <Link className='LinkNav' href={'/'}><GoPlus /> Vendez votre voiture</Link>
+          <Link className='LinkNav' href={'/admin'} ><GoGear/> Administration</Link>
+          <div style={{display:'flex', gap: '15px'}}> 
+            <Link href={route('login')}> <button className='btnLogin'>Connexion</button> </Link>
+            <Link href={route('register')}> <button className='btnRegister'>Inscription</button> </Link>
+          </div>
+          
         </div>
       )}
     </div>
