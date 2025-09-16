8a8689797a461835c39dea193d656f84002c7e87
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-09-16 15:21:06 +0200

    commn routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// HOME
 Route::get('/', function () {
     return Inertia::render("Home", [
         'user' => User::all(),
@@ -27,19 +28,23 @@ Route::get('/', function () {
     ]);
 })->name('homepage');
 
+// MAIL
 Route::get('/mail-test', function () {
     Mail::to('avroost@gmail.com')->send(new DemoMail());
     return 'Spécial retour pour Gary.';
 })->name('mail');
+// CAR AVEC MIDDLEWARE CONNECTE
 Route::middleware('auth')->group(function () {
     Route::get('/cars/create', [CarController::class, 'create'])->name('cars.create');
     Route::post('/cars', [CarController::class, 'store'])->name('cars.store');
     Route::delete('/cars/{car}', [CarController::class, 'destroy'])->name('cars.destroy');
 });
+// BRAND AVEC MIDDLEWARE ROLE ID 2 OU 3 
 Route::middleware(['auth', 'admin_or_modo'])->group(function () {
     Route::get('/brands/create', [BrandController::class, 'create'])->name('brands.create');
     Route::post('/brands', [BrandController::class, 'store'])->name('brands.store');
 });
+// ROUTE SHOW CAR
 Route::get('/cars/{id}', function ($id) {
     $car = Car::with(['brand','fuel','user'])->findOrFail($id);
     return Inertia::render("Show", [
@@ -49,16 +54,19 @@ Route::get('/cars/{id}', function ($id) {
         ]
     ]);
 })->name('cars.show');
+// DASHBOARD (PAS UTILISE)
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// ROUTE PAGE ADMIN AVEC MIDDLEWARE ROLE ID 3
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
     Route::patch('/admin/users/{user}/role', [AdminController::class, 'updateUserRole'])->name('admin.update-role');
     Route::delete('/admin/users/{user}', [AdminController::class, 'deleteUser'])->name('admin.delete-user');
 });
 
+// PAS UTILISE
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
