3f010bd8482d3e9e4050a43d42f4d2431464212f
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-09-17 11:35:48 +0200

    ajout react icons footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -1,6 +1,10 @@
 import { Link } from '@inertiajs/react';
 import React from 'react'
 import { LuCar } from "react-icons/lu";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+
 import '../../css/app.css'
 export default function Footer () {
   return (
@@ -10,6 +14,11 @@ export default function Footer () {
           <Link style={{textDecoration:"none", color:'white'}} className='div1FooterP'><LuCar/> Automarket</Link>
           <Link href={'/'} style={{textDecoration:"none", color:'white'}} className='div1FooterLink'>Catalogue</Link>
           <Link href={route('cars.create')} style={{textDecoration:"none", color:'white'}} className='div1FooterLink'>Vendez votre voiture</Link>
+        </div>
+        <div className='div1Footer ' style={{color:'white', paddingTop:'0px', marginTop:'0px'}}>
+          <Link href={'https://www.linkedin.com/feed/'}><FaLinkedinIn className='SMIcons' /> </Link>
+          <Link href={'https://github.com/ArthurVRoost'}><FaGithub className='SMIcons' /> </Link>
+          <Link href={'https://www.facebook.com/'}><FaFacebookF className='SMIcons' /></Link>
         </div>
       </div>
       <div className='div2Footer'>
